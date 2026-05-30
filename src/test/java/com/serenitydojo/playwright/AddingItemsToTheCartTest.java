2d65796de1dbf3a5ef2cada8b9a26cea757c8b6c
Kaustubh69 <kaustubhc948@gmail.com>
2026-05-30 21:08:12 +0530

    Test using locators

---
+++ src/test/java/com/serenitydojo/playwright/AddingItemsToTheCartTest.java
@@ -0,0 +1,66 @@
+package com.serenitydojo.playwright;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.assertions.PlaywrightAssertions;
+import com.microsoft.playwright.options.AriaRole;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+
+public class AddingItemsToTheCartTest {
+
+    private static Playwright playwright;
+    private static Browser browser;
+    private static BrowserContext browserContext;
+
+    Page page;
+
+    @BeforeAll
+    public static void setUpBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(
+                new BrowserType.LaunchOptions()
+                        .setHeadless(false)
+                        .setArgs(Arrays.asList("--no-sandbox","--disable-extensions","--disable-gpu"))
+                        .setSlowMo(500)
+        );
+        playwright.selectors().setTestIdAttribute("data-test");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        browserContext = browser.newContext();
+        page = browserContext.newPage();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        browser.close();
+        playwright.close();
+    }
+
+    @DisplayName("Search for pliers")
+    @Test
+    void searchForPliers() {
+        page.navigate("https://practicesoftwaretesting.com");
+        page.getByPlaceholder("Search").fill("Pliers"); //get text bar
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Search")).click(); //enter text
+
+        assertThat(page.locator(".card")).hasCount(4); //assert result
+        List<String> productNames = page.getByTestId("product-name").allTextContents();
+        Assertions.assertThat(productNames).allMatch(name -> name.contains("Pliers")); //asserts that all results have Pliers
+
+        Locator outOfStockItem = page.locator(".card")
+                .filter(new Locator.FilterOptions().setHasText("Out of stock"))
+                .getByTestId("product-name");
+        assertThat(outOfStockItem).hasCount(1);
+        assertThat(outOfStockItem).hasText("Long Nose Pliers");
+
+    } 
+
+}
